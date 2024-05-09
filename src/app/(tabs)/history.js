7ee1ecd0fa37885fd50cd98666cdccb9c836bca0
bravindan <danielbravin0@gmail.com>
2024-05-09 16:43:15 +0300

    Added and configured nativewind styling library

--- src/app/(tabs)/history.js
+++ src/app/(tabs)/history.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {  Text, View, TouchableOpacity } from 'react-native';
 
 export default function History() {
   return (
-    <View style={styles.container}>
-      <Text>History Screen</Text>
+    <View className="flex-1 p-2">
+      <Text className="font-semibold text-center">History</Text>
       <StatusBar style="dark" backgroundColor=''/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/app/(tabs)/index.js
+++ src/app/(tabs)/index.js
@@ -2,20 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function History() {
+export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 p-2">
       <StatusBar style="dark" backgroundColor=''/>
-      <Text>Home Screen</Text>
+      <Text className="font-semibold text-xl text-center">Boost your life, Fast and track the time with ease</Text>
+      <View className=" flex-1 mt-2 rounded-md" style={{backgroundColor:'#9999ff'}}>
+          <Text className="text-white font-semibold text-center mt-2 text-lg">Fasting</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container:{
+//     backgroundColor:'#9999ff'
+//   }
+// })
